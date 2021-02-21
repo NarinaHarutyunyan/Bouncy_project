c2c77d5cda03158a7ecb9a8d7702cdc08dcbed93
NarinaHarutyunyan <nari_har@yahoo.com>
2021-02-22 03:37:54 +0400

    my landing page

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(function(){
+  $('.team_slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+  $("#progress").progressBar({
+    width: 500,
+    height: 20,
+    percent: 17,
+    showPercent: true,
+    split: 1
+   });
+
+})
+$(function(){
+  $('.test_slide').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+  $("#progress").progressBar({
+    width: 500,
+    height: 20,
+    percent: 17,
+    showPercent: true,
+    split: 1
+   });
+
+})
+
